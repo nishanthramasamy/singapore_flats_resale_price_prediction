99ae1294f65782e40732c01e2acbde5a66c4e6a3
Nishanth Ramasamy <59798118+nishanthramasamy@users.noreply.github.com>
2024-04-16 19:34:41 +0530

    Update singapore_final_model.py

--- singapore_final_model.py
+++ singapore_final_model.py
@@ -9,6 +9,7 @@ from scipy.stats import randint
 from sklearn.model_selection import RandomizedSearchCV
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 
 tab1, tab2 = st.tabs(["Home", "Calculate"])
 
@@ -28,69 +29,68 @@ with tab1:
     st.write("Check out the calculate tab!!!!!")
 
 
-    #st.write("_Start predicting_")
-    df1 = pd.read_csv(rf"data_1990_1999.csv")
-    df2 = pd.read_csv(rf"data_2000_2012.csv")
-    df3 = pd.read_csv(rf"data_2012_2014.csv")
-    df4 = pd.read_csv(rf"data_2015_2016.csv")
-    df5 = pd.read_csv(rf"data_2017_present.csv")
-    df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
+df1 = pd.read_csv(rf"data_1990_1999.csv")
+df2 = pd.read_csv(rf"data_2000_2012.csv")
+df3 = pd.read_csv(rf"data_2012_2014.csv")
+df4 = pd.read_csv(rf"data_2015_2016.csv")
+df5 = pd.read_csv(rf"data_2017_present.csv")
+df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
 
-    #preprocessing && Feature Engineering
-    df['resale_price'].fillna(df['resale_price'].mean(), inplace=True)
-    df['town'].fillna(method='ffill', inplace=True)
-    df['flat_type_modified'] = [x.replace('-', ' ') for x in df['flat_type']]
+#preprocessing && Feature Engineering
+df['resale_price'].fillna(df['resale_price'].mean(), inplace=True)
+df['town'].fillna(method='ffill', inplace=True)
+df['flat_type_modified'] = [x.replace('-', ' ') for x in df['flat_type']]
 
-    df['year'] = [x.split('-')[0] for x in df['month']]
-    df['year'] = pd.to_numeric(df['year'])
-    df['lease_commence_date'] = pd.to_numeric(df['lease_commence_date'])
+df['year'] = [x.split('-')[0] for x in df['month']]
+df['year'] = pd.to_numeric(df['year'])
+df['lease_commence_date'] = pd.to_numeric(df['lease_commence_date'])
 
-    df['remaining_lease'].fillna(99 - (df['year'] - df['lease_commence_date']), inplace=True)
-    df['remaining_lease'] = df['remaining_lease'].astype(str).str.split(' ').str[0]
-    df['remaining_lease'] = pd.to_numeric(df['remaining_lease'])
+df['remaining_lease'].fillna(99 - (df['year'] - df['lease_commence_date']), inplace=True)
+df['remaining_lease'] = df['remaining_lease'].astype(str).str.split(' ').str[0]
+df['remaining_lease'] = pd.to_numeric(df['remaining_lease'])
 
+
+def get_encoded(data, df):
+    encoder = LabelEncoder()
+    encoder.fit(df)
+    # encoded_column = encoder.transform(df)
+    encoded_data = encoder.transform([data])
+
+    return  encoded_data[0]
+
+values = df.columns
+
+features = []
+
+features_dict = {
+                'town' : "",
+                'flat_type': "",  
+                'flat_model': "",
+                'floor_area_sqm' : "",
+                'year' : "",
+                'remaining_lease': ""
+}
 
 with tab2:
-
-    #Function to encode categorical variables::
-    def get_encoded(df, data):
-        encoder = LabelEncoder()
-        encoder.fit(df)
-        encoded_column = encoder.transform(df)
-        encoded_data = encoder.transform([data])
-
-        return encoded_column, encoded_data[0]
-
-    values = df.columns
-
-    features = []
-
-    features_dict = {
-                    'town' : "",
-                    'flat_type': "",  
-                    'flat_model': "",
-                    'floor_area_sqm' : "",
-                    'year' : "",
-                    'remaining_lease': ""
-    }
-
-    #selecting the features
+#selecting the features
     towns = df['town'].unique().tolist()
     town_selected = st.selectbox("Choose the town ", towns)
-    features_dict['town'] = town_selected
-    df['encoded_town'], features_dict['town'] = get_encoded(df['town'],town_selected)
+    features_dict['town'] = get_encoded(town_selected, df['town'])
+    #df['encoded_town'], features_dict['town'] = get_encoded(df['town'],town_selected)
 
 
 
     flat_types = df['flat_type_modified'].unique()
     flat_type = st.selectbox("choose the flat type", flat_types)
-    df['encoded_flat_type'], features_dict['flat_type'] = get_encoded(df['flat_type_modified'], flat_type)
+    features_dict['flat_type'] = get_encoded(flat_type, df['flat_type_modified'])
+    #df['encoded_flat_type'], features_dict['flat_type'] = get_encoded(df['flat_type_modified'], flat_type)
         
 
 
     flat_models = df['flat_model'].unique()
     flat_model = st.selectbox("choose the flat model", flat_models)
-    df['encoded_flat_model'], features_dict['flat_model'] = get_encoded(df['flat_model'], flat_model)
+    features_dict['flat_model'] = get_encoded(flat_model, df['flat_model'])
+    #df['encoded_flat_model'], features_dict['flat_model'] = get_encoded(df['flat_model'], flat_model)
         
         
     floors = df['floor_area_sqm'].unique()
@@ -115,112 +115,23 @@ with tab2:
         lease = 0
     features_dict['remaining_lease'] = int(lease)
 
-    x = df[['encoded_town', 'encoded_flat_type', 'encoded_flat_model', 'floor_area_sqm', 'year', 'remaining_lease']]
-    y = df['resale_price']
 
-
-    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=42)
-    dt_regressor = RandomForestRegressor()
-    decision_model = RandomForestRegressor(random_state=42)
-    decision_model.fit(x_train, y_train)
-
-    #PICKLING:::
-    with open("model.pk1", "wb") as file:
-        pickle.dump(decision_model, file)
-
-    with open("model.pk1", "rb") as file:
-        unloaded = pickle.load(file)
-
-    y_pred_random = unloaded.predict(x_test)
-    mae_random = mean_absolute_error(y_pred_random, y_test)
-    r2score_random = r2_score(y_pred_random, y_test)
+    loaded_model = joblib.load('random_model.pkl.gz')
 
     predict = st.button("PREDICT")
     if predict:            
 
         
-            #st.write("randomforest mae", mae)
+        #st.write("randomforest mae", mae)
 
-            #Predicting unknown choice from user
-            x_final = []
-            #st.write("features_dict.values=", features_dict.values())
-            for i in list(features_dict.values()):
-                if i :
-                    x_final.append(i)
-            #st.write("x_final= ", x_final) 
-            final_price = decision_model.predict([x_final])
-            final_price = round(final_price[0])
-            st.write(f"The resale value of flat of your choice is :blue[{final_price}] GSD")
-
-
-# with tab3:
-#     st.write("This section gives you information behind the model. Check below for more details")
-
-
-#     st.header("Parameter Selection:")
-#     st.write("The dataset had many features like street name, blocks, town etc. But we use certain features which help in creating model.")
-
-#     st.write("Click below to visualise relationship between features")
-
-#     st1, st2, st3, st4 = st.columns([5,5,5,5])
-#     with st1:
-#         visual = st.button("Click here to see the charts")
-   
-#      #EDA::: Visualising the features with respect to target variable to understand the relationship
-#     if visual:
-#         col1, col2= st.columns([5,5])
-#         with col1:
-#             fig, ax = plt.subplots()
-#             sns.scatterplot(data=df, x='year', y='resale_price', ax=ax)
-#             plt.xticks(rotation=90)
-#             st.pyplot(fig)
-#         with col2:
-#             fig, ax = plt.subplots()
-#             plt.figure(figsize=(10, 6))
-#             sns.scatterplot(data=df, x='flat_model', y='resale_price', ax=ax)
-#             plt.xticks(rotation=90)
-#             st.pyplot(fig)
-#         col3, col4 = st.columns([5,5])
-#         with col3:
-#             fig, ax = plt.subplots()
-#             plt.figure(figsize=(10, 6))
-#             sns.scatterplot(data=df, x='town', y='resale_price', ax=ax)
-#             plt.xticks(rotation=90)
-#             st.pyplot(fig)
-#         with col4:
-#             fig, ax = plt.subplots()
-#             sns.scatterplot(data=df, x='flat_type_modified', y='resale_price', ax=ax)
-#             plt.xticks(rotation=90) 
-#             st.pyplot(fig)
-
-#     st.write("Feature importance:")
-#     importance_scores = decision_model.feature_importances_
-#     scores = []
-#     col = features_dict.keys
-#     for x in importance_scores:
-#         scores.append(x)
-#     data = {"features":["Town", "Flat type", "Flat model", "Floor Area", "Year", "Remaining Lease"],
-#             "importance score": scores}
-#     sc = pd.DataFrame(data)
-#     st.dataframe(sc)
-
-#     st.header("Model used:")
-#     #DecisionTree:::
-#     x = df[['encoded_town', 'encoded_flat_type', 'encoded_flat_model', 'floor_area_sqm', 'year', 'remaining_lease']]
-#     y = df['resale_price']
-#     x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=42)
-#     decision_model = DecisionTreeRegressor(random_state=42)
-#     decision_model.fit(x_train, y_train)
-#     y_pred = decision_model.predict(x_test)
-#     mae = mean_absolute_error(y_pred, y_test)
-#     r2 = r2_score(y_test, y_pred)
-    
-
-#     st.write("The model is trained on :orange[RandomForestRegressor]. Initially :orange[LinearRegression], :orange[DecisionTreeRegressor] where tried but their performance was low compared to Randomforest")
-#     st.write(f"DecisionTree : R^2 Score: {r2:.2f}")
-#     st.write("Decision Tree - MAE:", mae)
-
-#     #RandomForest:
-#     st.write(f"MAE - RandomForest: {mae_random}")
-#     st.write(f"R^2 Score - RandomForest: {r2score_random:.2f}")
+        #Predicting unknown choice from user
+        x_final = []
+        #st.write("features_dict.values=", features_dict.values())
+        for i in list(features_dict.values()):
+            if i :
+                x_final.append(i)
+        #st.write("x_final= ", x_final) 
+        final_price = loaded_model.predict([x_final])
+        final_price = round(final_price[0])
+        st.write(f"The resale value of flat of your choice is :blue[{final_price}] GSD")
 
